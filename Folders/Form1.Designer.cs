f02bbb112e4aa2faf90af42cf56f877040c70c94
balashov <alex.nsk.convexbytes@gmail.com>
2023-03-05 15:51:43 +0300

    Add project files.

---
+++ Folders/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Folders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialogSelectRootFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxDeloNumber = new System.Windows.Forms.TextBox();
+            this.DeloNumber = new System.Windows.Forms.Label();
+            this.TomNumbers = new System.Windows.Forms.Label();
+            this.textBoxTomNunbers = new System.Windows.Forms.TextBox();
+            this.buttonGeneratingFolders = new System.Windows.Forms.Button();
+            this.panelWithControls = new System.Windows.Forms.Panel();
+            this.panelCalculator = new System.Windows.Forms.Panel();
+            this.labelTotalPages = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTotalTomov = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTotalDels = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelCalculator = new System.Windows.Forms.Label();
+            this.buttonCalculatorAllDel = new System.Windows.Forms.Button();
+            this.buttonFolderBrowserDialog = new System.Windows.Forms.Button();
+            this.labelSelectedRootFolder = new System.Windows.Forms.Label();
+            this.labelRootFolderValue = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelWithControls.SuspendLayout();
+            this.panelCalculator.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxDeloNumber
+            // 
+            this.textBoxDeloNumber.Location = new System.Drawing.Point(15, 37);
+            this.textBoxDeloNumber.Name = "textBoxDeloNumber";
+            this.textBoxDeloNumber.Size = new System.Drawing.Size(332, 20);
+            this.textBoxDeloNumber.TabIndex = 0;
+            this.textBoxDeloNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxDeloNumber_KeyPress);
+            this.textBoxDeloNumber.Leave += new System.EventHandler(this.textBoxDeloNumber_Leave);
+            // 
+            // DeloNumber
+            // 
+            this.DeloNumber.AutoSize = true;
+            this.DeloNumber.Location = new System.Drawing.Point(12, 11);
+            this.DeloNumber.Name = "DeloNumber";
+            this.DeloNumber.Size = new System.Drawing.Size(226, 13);
+            this.DeloNumber.TabIndex = 1;
+            this.DeloNumber.Text = "Введите номер дела";
+            // 
+            // TomNumbers
+            // 
+            this.TomNumbers.AutoSize = true;
+            this.TomNumbers.Location = new System.Drawing.Point(12, 73);
+            this.TomNumbers.Name = "TomNumbers";
+            this.TomNumbers.Size = new System.Drawing.Size(200, 13);
+            this.TomNumbers.TabIndex = 2;
+            this.TomNumbers.Text = "введите номера томов через запятую";
+            // 
+            // textBoxTomNunbers
+            // 
+            this.textBoxTomNunbers.Location = new System.Drawing.Point(15, 103);
+            this.textBoxTomNunbers.Name = "textBoxTomNunbers";
+            this.textBoxTomNunbers.Size = new System.Drawing.Size(332, 20);
+            this.textBoxTomNunbers.TabIndex = 3;
+            this.textBoxTomNunbers.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxTomNunbers_KeyPress);
+            this.textBoxTomNunbers.Leave += new System.EventHandler(this.textBoxTomNunbers_Leave);
+            // 
+            // buttonGeneratingFolders
+            // 
+            this.buttonGeneratingFolders.Location = new System.Drawing.Point(15, 139);
+            this.buttonGeneratingFolders.Name = "buttonGeneratingFolders";
+            this.buttonGeneratingFolders.Size = new System.Drawing.Size(332, 30);
+            this.buttonGeneratingFolders.TabIndex = 4;
+            this.buttonGeneratingFolders.Text = "Создать папки";
+            this.buttonGeneratingFolders.UseVisualStyleBackColor = true;
+            this.buttonGeneratingFolders.Click += new System.EventHandler(this.buttonGeneratingFolders_Click);
+            // 
+            // panelWithControls
+            // 
+            this.panelWithControls.Controls.Add(this.DeloNumber);
+            this.panelWithControls.Controls.Add(this.textBoxTomNunbers);
+            this.panelWithControls.Controls.Add(this.buttonGeneratingFolders);
+            this.panelWithControls.Controls.Add(this.textBoxDeloNumber);
+            this.panelWithControls.Controls.Add(this.TomNumbers);
+            this.panelWithControls.Location = new System.Drawing.Point(12, 12);
+            this.panelWithControls.Name = "panelWithControls";
+            this.panelWithControls.Size = new System.Drawing.Size(362, 178);
+            this.panelWithControls.TabIndex = 5;
+            this.panelWithControls.Visible = false;
+            // 
+            // panelCalculator
+            // 
+            this.panelCalculator.Controls.Add(this.labelCalculator);
+            this.panelCalculator.Controls.Add(this.buttonCalculatorAllDel);
+            this.panelCalculator.Controls.Add(this.labelTotalPages);
+            this.panelCalculator.Controls.Add(this.label3);
+            this.panelCalculator.Controls.Add(this.labelTotalTomov);
+            this.panelCalculator.Controls.Add(this.label1);
+            this.panelCalculator.Controls.Add(this.labelTotalDels);
+            this.panelCalculator.Controls.Add(this.label2);
+            this.panelCalculator.Location = new System.Drawing.Point(12, 196);
+            this.panelCalculator.Name = "panelCalculator";
+            this.panelCalculator.Size = new System.Drawing.Size(362, 93);
+            this.panelCalculator.TabIndex = 8;
+            // 
+            // labelTotalPages
+            // 
+            this.labelTotalPages.AutoSize = true;
+            this.labelTotalPages.Location = new System.Drawing.Point(237, 58);
+            this.labelTotalPages.Name = "labelTotalPages";
+            this.labelTotalPages.Size = new System.Drawing.Size(13, 13);
+            this.labelTotalPages.TabIndex = 12;
+            this.labelTotalPages.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "всего страниц";
+            // 
+            // labelTotalTomov
+            // 
+            this.labelTotalTomov.AutoSize = true;
+            this.labelTotalTomov.Location = new System.Drawing.Point(237, 45);
+            this.labelTotalTomov.Name = "labelTotalTomov";
+            this.labelTotalTomov.Size = new System.Drawing.Size(13, 13);
+            this.labelTotalTomov.TabIndex = 11;
+            this.labelTotalTomov.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "всего дел";
+            // 
+            // labelTotalDels
+            // 
+            this.labelTotalDels.AutoSize = true;
+            this.labelTotalDels.Location = new System.Drawing.Point(237, 32);
+            this.labelTotalDels.Name = "labelTotalDels";
+            this.labelTotalDels.Size = new System.Drawing.Size(13, 13);
+            this.labelTotalDels.TabIndex = 10;
+            this.labelTotalDels.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "всего томов";
+            // 
+            // labelCalculator
+            // 
+            this.labelCalculator.AutoSize = true;
+            this.labelCalculator.Location = new System.Drawing.Point(9, 9);
+            this.labelCalculator.Name = "labelCalculator";
+            this.labelCalculator.Size = new System.Drawing.Size(328, 13);
+            this.labelCalculator.TabIndex = 6;
+            this.labelCalculator.Text = "Подсчиать общее количества дел, томов и страниц за сегодня";
+            // 
+            // buttonCalculatorAllDel
+            // 
+            this.buttonCalculatorAllDel.Location = new System.Drawing.Point(9, 27);
+            this.buttonCalculatorAllDel.Name = "buttonCalculatorAllDel";
+            this.buttonCalculatorAllDel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculatorAllDel.TabIndex = 5;
+            this.buttonCalculatorAllDel.Text = "Подсчитать";
+            this.buttonCalculatorAllDel.UseVisualStyleBackColor = true;
+            this.buttonCalculatorAllDel.Click += new System.EventHandler(this.buttonCalculatorAllDel_Click);
+            // 
+            // buttonFolderBrowserDialog
+            // 
+            this.buttonFolderBrowserDialog.Location = new System.Drawing.Point(3, 33);
+            this.buttonFolderBrowserDialog.Name = "buttonFolderBrowserDialog";
+            this.buttonFolderBrowserDialog.Size = new System.Drawing.Size(121, 23);
+            this.buttonFolderBrowserDialog.TabIndex = 5;
+            this.buttonFolderBrowserDialog.Text = "Изменить";
+            this.buttonFolderBrowserDialog.UseVisualStyleBackColor = true;
+            this.buttonFolderBrowserDialog.Click += new System.EventHandler(this.buttonFolderBrowserDialog_Click);
+            // 
+            // labelSelectedRootFolder
+            // 
+            this.labelSelectedRootFolder.AutoSize = true;
+            this.labelSelectedRootFolder.Location = new System.Drawing.Point(12, 0);
+            this.labelSelectedRootFolder.Name = "labelSelectedRootFolder";
+            this.labelSelectedRootFolder.Size = new System.Drawing.Size(191, 13);
+            this.labelSelectedRootFolder.TabIndex = 6;
+            this.labelSelectedRootFolder.Text = "Выберите папку с вашими сканами ";
+            // 
+            // labelRootFolderValue
+            // 
+            this.labelRootFolderValue.AutoSize = true;
+            this.labelRootFolderValue.Location = new System.Drawing.Point(145, 38);
+            this.labelRootFolderValue.Name = "labelRootFolderValue";
+            this.labelRootFolderValue.Size = new System.Drawing.Size(141, 13);
+            this.labelRootFolderValue.TabIndex = 7;
+            this.labelRootFolderValue.Text = "Имя вашей рабочей папки";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonFolderBrowserDialog);
+            this.panel1.Controls.Add(this.labelRootFolderValue);
+            this.panel1.Controls.Add(this.labelSelectedRootFolder);
+            this.panel1.Location = new System.Drawing.Point(12, 295);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 66);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 369);
+            this.Controls.Add(this.panelCalculator);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelWithControls);
+            this.Name = "Form1";
+            this.Text = "Генератор папок v.1.0";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panelWithControls.ResumeLayout(false);
+            this.panelWithControls.PerformLayout();
+            this.panelCalculator.ResumeLayout(false);
+            this.panelCalculator.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogSelectRootFolder;
+        private System.Windows.Forms.TextBox textBoxDeloNumber;
+        private System.Windows.Forms.Label DeloNumber;
+        private System.Windows.Forms.Label TomNumbers;
+        private System.Windows.Forms.TextBox textBoxTomNunbers;
+        private System.Windows.Forms.Button buttonGeneratingFolders;
+        private System.Windows.Forms.Panel panelWithControls;
+        private System.Windows.Forms.Button buttonFolderBrowserDialog;
+        private System.Windows.Forms.Label labelSelectedRootFolder;
+        private System.Windows.Forms.Label labelRootFolderValue;
+        private System.Windows.Forms.Panel panelCalculator;
+        private System.Windows.Forms.Label labelTotalPages;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelTotalTomov;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTotalDels;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelCalculator;
+        private System.Windows.Forms.Button buttonCalculatorAllDel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Folders/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Folders
+{
+    public partial class Form1 : Form
+    {
+        private String rootFolderValue;
+        private static readonly String rootFolderName = "root.txt";
+        private GeneratedFoldersStore _generatedFoldersStore;
+        private readonly Regex  regexWithNumbersAndComa = new Regex("^[\\d,]+$");
+        private readonly Regex  regexWithNumbersAndSpace = new Regex("^[\\d\\s]+$");
+        private readonly Regex  regexOnlyWithNumbers = new Regex("^[\\d]+$");
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists(rootFolderName)){
+                String[] fileLines = File.ReadAllLines(rootFolderName);
+                rootFolderValue = fileLines[0];
+                panelWithControls.Visible = true;
+                labelSelectedRootFolder.Text = rootFolderValue;
+                
+            }
+            else
+            {
+                callSelectFolderDialog();
+            }
+        }
+
+        private void callSelectFolderDialog()
+        {
+            if (this.folderBrowserDialogSelectRootFolder.ShowDialog() == DialogResult.OK)
+            {
+                rootFolderValue = folderBrowserDialogSelectRootFolder.SelectedPath;
+                File.Delete(rootFolderName);
+                File.WriteAllText(rootFolderName, rootFolderValue);
+                panelWithControls.Visible = true;
+                labelSelectedRootFolder.Text = rootFolderValue;
+                
+            }
+        }
+
+        private void buttonFolderBrowserDialog_Click(object sender, EventArgs e)
+        {
+            callSelectFolderDialog();
+        }
+
+        private void buttonGeneratingFolders_Click(object sender, EventArgs e)
+        {
+            if ( panelWithControls.ContainsFocus )
+            {
+                if(textBoxTomNunbers.Text.Length == 0)
+                {
+                    var result = MessageBox.Show("Введите номера томов через запятые!!!");
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxTomNunbers.Focus();
+                    }
+                }
+                else
+                {
+                    _generatedFoldersStore.Generate();
+                    _generatedFoldersStore = null;
+                    textBoxDeloNumber.Clear();
+                    textBoxTomNunbers.Clear();
+                }
+            }
+        }
+
+        private void textBoxDeloNumber_Leave(object sender, EventArgs e)
+        {
+            if (panelWithControls.ContainsFocus)
+            {
+                if (textBoxDeloNumber.Text.Length > 0)
+                {
+                    if (regexWithNumbersAndSpace.IsMatch(textBoxDeloNumber.Text))
+                    {
+                        _generatedFoldersStore = new GeneratedFoldersStore(rootFolderValue, textBoxDeloNumber.Text);
+
+                    }
+                    else
+                    {
+                        var result = MessageBox.Show("В имени дела должны быть только ЦЫФРЫ и ПРОБЕЛЫ");
+                        if (result == DialogResult.OK)
+                        {
+                            textBoxDeloNumber.Clear();
+                            textBoxDeloNumber.Focus();
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    var result = MessageBox.Show("Вы забыли ввести номер дела");
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxDeloNumber.Focus();
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            textBoxDeloNumber.Focus();
+        }
+
+
+        private void textBoxTomNunbers_Leave(object sender, EventArgs e)
+        {
+            if (panelWithControls.ContainsFocus)
+            {
+                if (textBoxTomNunbers.Text.Length > 0)
+                {
+                    if (regexWithNumbersAndComa.IsMatch(textBoxTomNunbers.Text))    
+                    {
+
+                        String deloNumbersText = textBoxTomNunbers.Text;
+                        List<int> indexes = deloNumbersText.Split(',').Select(s => int.Parse(s)).ToList();
+                        _generatedFoldersStore.tomeIndexes.AddRange(indexes);
+                    }else if (regexOnlyWithNumbers.IsMatch(textBoxTomNunbers.Text))
+                    {
+                        int index = int.Parse(textBoxDeloNumber.Text);
+                        _generatedFoldersStore.tomeIndexes.Add(index);
+                    }
+                    else
+                    {
+                        var result = MessageBox.Show("Индексы томов могут содержать только ЗАПЯТЫЕ и ЦИФРЫ!!!. Введитe номера томов через запятые!!!");
+                        if (result == DialogResult.OK)
+                        {
+                            textBoxTomNunbers.Clear();  
+                            textBoxTomNunbers.Focus();
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    var result = MessageBox.Show("Введите номера томов через запятые!!!");
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxTomNunbers.Focus();
+                    }
+                }
+            }
+
+        }
+
+        private void textBoxDeloNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)13)
+            {
+                if (regexWithNumbersAndSpace.IsMatch(textBoxDeloNumber.Text))
+                {
+                    _generatedFoldersStore = new GeneratedFoldersStore(rootFolderValue, textBoxDeloNumber.Text.Trim());
+                    textBoxTomNunbers.Focus();
+
+                }
+                else
+                {
+                    var result = MessageBox.Show("В имени дела должны быть только ЦЫФРЫ и ПРОБЕЛЫ");
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxDeloNumber.Clear();
+                        textBoxDeloNumber.Focus();
+                    }
+                }
+                
+            }
+        }
+
+        private void textBoxTomNunbers_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (textBoxTomNunbers.Text.Length > 0)
+                {
+                    if (regexWithNumbersAndComa.IsMatch(textBoxTomNunbers.Text))
+                    {
+
+                        String deloNumbersText = textBoxTomNunbers.Text.Trim();
+                        List<String> indexesS = deloNumbersText.Split(',').ToList();
+                        List<int> indexes = indexesS.Where(w => w.Length > 0).Select(s => int.Parse(s)).ToList();
+                        _generatedFoldersStore.tomeIndexes.AddRange(indexes);
+                        buttonGeneratingFolders.PerformClick();
+                    }
+                    else if (regexOnlyWithNumbers.IsMatch(textBoxTomNunbers.Text))
+                    {
+                        int index = int.Parse(textBoxDeloNumber.Text.Trim());
+                        _generatedFoldersStore.tomeIndexes.Add(index);
+                        buttonGeneratingFolders.PerformClick();
+                    }
+                    else
+                    {
+                        var result = MessageBox.Show("Индексы томов могут содержать только ЗАПЯТЫЕ и ЦИФРЫ!!!. Введитe номера томов через запятые!!!");
+                        if (result == DialogResult.OK)
+                        {
+                            textBoxTomNunbers.Clear();
+                            textBoxTomNunbers.Focus();
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    var result = MessageBox.Show("Введите номера томов через запятые!!!");
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxTomNunbers.Focus();
+                    }
+                }
+            }
+        }
+
+        private void buttonCalculatorAllDel_Click(object sender, EventArgs e)
+        {
+            TotalFoldersCalculator tfc = new TotalFoldersCalculator(rootFolderValue);
+            if (tfc.isFilesTodays())
+            {
+                TotalFoldersCalculator calculated = tfc.calculate();
+                labelTotalDels.Text = calculated.allDelos.ToString();
+                labelTotalTomov.Text = calculated.allTomes.ToString();
+                labelTotalPages.Text = calculated.allPages.ToString();
+            }
+            else
+            {
+                var result = MessageBox.Show("За сегодня файлов на найдено");
+                if (result == DialogResult.OK)
+                {
+                    textBoxTomNunbers.Focus();
+                }
+            }
+
+        }
+    }
+
+    class GeneratedFoldersStore
+    {
+        private String deloName;
+        private String rootName;
+        private String localDate;
+        private const String tomeIndexTemplate = "Том_{0}скан";
+
+
+        public GeneratedFoldersStore(string rootName, string deloName)
+        {
+            this.deloName = deloName;
+            this.rootName = rootName;
+            this.localDate = DateTime.Now.Date.ToString("dd.MM.yyyy");
+        }
+
+
+        public readonly List<int> tomeIndexes = new List<int>();
+
+        internal void Generate()
+        {
+         
+            List<String> folders = tomeIndexes.Select(i =>
+              {
+                  String tomeName = String.Format(tomeIndexTemplate, i);
+                  return $"{rootName}\\{localDate}\\{deloName}\\{tomeName}";
+              }).ToList();
+
+            
+            for (int i = 0; i < folders.Count; i++)
+            {
+                try
+                {
+                    Directory.CreateDirectory(folders[i]);
+                }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+
+            MessageBox.Show("Нужные папки были созданы");
+        }
+    }
+
+    class TotalFoldersCalculator
+    {
+        private String rootName;
+        private String localDate;
+
+        public int allDelos;
+        public int allTomes;
+        public int allPages;
+
+        String[] deloFolders;
+        String[] tomesFolders;
+        String[] pdfs;
+
+        public TotalFoldersCalculator(string rootName)
+        {
+            this.rootName = rootName;
+            this.localDate = DateTime.Now.Date.ToString("dd.MM.yyyy");
+        }
+
+        internal bool isFilesTodays() {
+            try
+            {
+                String todayFolder = Path.Combine(rootName, localDate);
+                deloFolders = Directory.GetDirectories(todayFolder, "*", SearchOption.TopDirectoryOnly);
+                tomesFolders = deloFolders.Select(s => Directory.GetDirectories(s, "*", SearchOption.TopDirectoryOnly)).SelectMany(s => s).ToArray();
+                pdfs = tomesFolders.Select(s => Directory.GetFiles(s, "*.pdf")).SelectMany(s => s).ToArray();
+                return pdfs.Length > 0;
+            }catch(Exception ex) { return false; }
+        }
+
+
+        internal TotalFoldersCalculator calculate()
+        {
+            int totalPages = pdfs.Select(s =>
+            {
+                using (StreamReader sr = new StreamReader(File.OpenRead(s)))
+                {
+                    Regex regex = new Regex(@"/Type\s*/Page[^s]");
+                    MatchCollection matches = regex.Matches(sr.ReadToEnd());
+
+                    return matches.Count;
+                }
+            }).Sum();
+
+            allDelos = deloFolders.Length;
+            allTomes = tomesFolders.Length;
+            allPages = totalPages;
+
+            return this;
+
+        }
+    }
+}
